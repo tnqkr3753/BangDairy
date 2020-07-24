cb04e3fb6a3736a4f91f7904fcb3a03276d08c01
ribtes <62416063+ribtes@users.noreply.github.com>
2020-07-24 09:34:25 +0900

    comiit version0.1.1 Lee

--- BangDairy/src/main/java/com/kosmo/bangdairy/service/AccountFormService.java
+++ BangDairy/src/main/java/com/kosmo/bangdairy/service/AccountFormService.java
@@ -1,5 +1,8 @@
 package com.kosmo.bangdairy.service;
 
+import com.kosmo.bangdairy.vo.AccountFormVO;
+
 public interface AccountFormService {
 	public int idCheck(String user_id);
+	public int insertUser(AccountFormVO vo);
 }
--- BangDairy/src/main/webapp/resources/js/account_form.js
+++ BangDairy/src/main/webapp/resources/js/account_form.js
@@ -17,9 +17,20 @@ $(document).ready(function(){
     
     var accountSubmit = function () {
     	var allCheckResult = false;
+    	var regId = /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{6,20}$/;
+    	var exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    	
+    	//조건1. 6~20 영문 대소문자
+    	//조건2. 최소 1개의 숫자 혹은 특수 문자를 포함해야 함
+    	
+    	var regPasswd = /^.*(?=.{6,20})(?=.*[0-9])(?=.*[a-zA-Z]).*$/; 
     	//아이디에 텍스트 입력시마다 ajax로 존재여부 결과를 가져온다.
     	$('#user_id').on("change keyup paste",function () {
     		var idCheck = $('#user_id').val();
+    		if (regId.test(idCheck)==false){
+    			$('#idCheckResult').html("영문자와 숫자로 이루어진 조합 6자리 이상만 가능합니다.");
+    			return
+    		}
     		if (idCheck.length==0){
     			$('#idCheckResult').html("");
     			return
@@ -40,7 +51,8 @@ $(document).ready(function(){
         			}
         		}
         	});
-    	});
+    	}); // 아이디 end
+    	
     	//이메일 정규식
     	$('#user_email').on("change keyup paste",function () {
     		var emailCheck = $('#user_email').val();
@@ -49,7 +61,7 @@ $(document).ready(function(){
     		}
     		else{
     			// 이메일 형식: 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식
-    			var exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    			
     			if(exptext.test(emailCheck) == false) { 
     				$('#eMailCheckResult').html("이메일 주소가 유효하지 않습니다.");
     				allCheckResult=false;
@@ -59,11 +71,41 @@ $(document).ready(function(){
     				allCheckResult=true;
     			}
     		}
-    	});
+    	}); // 이메일 정규식 end
     	
-		$('#account_body').submit(function () {
-			
+    	//비밀번호 정규식 체크
+    	$('#user_passwd').focusout(function () {
+    		var passWdCheck = $('#user_passwd').val();
+			if(regPasswd.test(passWdCheck)==false){
+				$('#passwdCheckResult').html("최소 1개의 숫자 또는 특수문자와 6~20자의 비밀번호를 입력해주세요");
+				allCheckResult=false;
+			}
+			else{
+				$('#passwdCheckResult').html("규격에 맞음");
+				allCheckResult=true;
+			}
 		});
+    	$('#user_passwd2').focusout(function () {
+    		var passWdCheck2 = $('#user_passwd2').val();
+			if(passWdCheck2 != $('#user_passwd').val()){
+				$('#passwdCheckResult2').html("비밀번호 불일치");
+				allCheckResult=false;
+			}
+			else{
+				$('#passwdCheckResult2').html("확인");
+				allCheckResult=true;
+			}
+		});
+    	
+    	$('#account_body').on("submit",function (e) {
+    		e.preventdefault();
+		    if (allCheckResult==true){
+		    	this.submit();
+		    }
+		});
+    	
+    	/*//성별 버튼 클릭시 정수형 변환
+    	$('.rbtn')*/
 	};
 	clAccount();
     accountSubmit();
