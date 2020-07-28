8456df4f9bcdbb0d71a1ac5ae5ead253dbe9402d
tnqkr3753 <tnqkr4610@gmail.com>
2020-07-28 22:29:16 +0900

    commit version0.7.728 park home

--- BangDairy/src/main/java/com/kosmo/bangdairy/interceptor/LoggingInterceptor.java
+++ BangDairy/src/main/java/com/kosmo/bangdairy/interceptor/LoggingInterceptor.java
@@ -1,8 +1,22 @@
 package com.kosmo.bangdairy.interceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class LoggingInterceptor {
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
 	public static final Logger logger = LogManager.getLogger("Interceptor");
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info(request.getRemoteAddr()+"\t"+request.getSession().getAttribute("userId")+"\t"+request.getServletPath()+"\t"+request.getHeader("referer")+"\t"+response.getLocale());
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	
 }
